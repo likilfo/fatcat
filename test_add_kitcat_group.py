ffdf53aeb130d1948a8115bebf8a25feb8766eae
oksana trizno <likikfo@mail.ru>
2019-11-05 23:43:41 +0700

    2nd task

--- test_add_kitcat_group.py
+++ test_add_kitcat_group.py
@@ -1,55 +1,59 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 class TestAddKitcatGroup(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox(
+            executable_path=r'C:\Windows\SysWOW64\geckodriver.exe')
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self):
         self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+
+    def login(self, wd, login, password):
+        wd.get("http://127.0.0.1/addressbook/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def login_by_admin(self, wd):
+        self.login(wd, login='admin', password='secret')
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_new_group(self, wd, group):
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
     def test_add_kitcat_group(self):
-        driver = self.driver
-        driver.get("http://127.0.0.1/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").submit()
-        driver.find_element_by_xpath("//html").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("kitcat")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_xpath("//body").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("kitcat group header")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("kitcat group footer")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
+        wd = self.wd
+        self.open_home_page()
+        self.login_by_admin(wd)
+        self.open_home_page(wd)
+        self.create_new_group(wd, Group(name='kitcat',
+                                        header='kitcat group header',
+                                        footer='kitcat group footer'))
+        self.return_to_group_page(wd)
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
